c9e0b451daebd4e3801b091ec28633f6bfab6eb5
Galen Gorski <32860978+galengorski@users.noreply.github.com>
2022-02-02 11:37:26 -0500

    add fertilizer dataset to basin_char and solar radiation to met data

--- 02_munge/src/merge_hydro_met_basin_char.R
+++ 02_munge/src/merge_hydro_met_basin_char.R
@@ -14,6 +14,8 @@
 library(tidyverse)
 # install.packages('magrittr')
 library(magrittr)
+ install.packages('netcdf4')
+library(netcdf4)
 #####
 #===================================================================================#
 
@@ -22,5 +24,14 @@ sites <- unique(hydro_data$site_no)
 hydro_data_temp <- hydro_data %>%
   filter(site_no == sites[i])
 met_data <- read_csv(paste0('01_fetch/out/met_data/',sites[i],'_met_data.csv'))
+
+nrow(hydro_data_temp) == nrow(met_data)
+hydro_met <- merge(hydro_data_temp, met_data, by.x = 'Date', by.y = 'date', all = TRUE) %>%
+  select(-...1)
+
 basin_char <- read_csv(paste0('01_fetch/out/basin_char/',sites[i],'_basin_char.csv'))
-land_cover <- read_csv(paste0('01_fetch/out/nlcd_data/land_cover_',sites[i],'.csv'))
+land_cover <- read.table(paste0('01_fetch/out/nlcd_data/land_cover_',sites[i],'.csv'), header = TRUE) %>%
+  mutate(characteristic_id = paste0('NLCD_', cat), characteristic_value = value, percent_nodata = NA) %>%
+  select(characteristic_id, characteristic_value, percent_nodata)
+
+basin_char_lc <- rbind(basin_char, land_cover)
