854d48e702483d2135b0eae072e9a22f07628eb9
Galen Gorski <32860978+galengorski@users.noreply.github.com>
2023-02-02 07:55:40 -0500

    add clustered model run functionality

--- 03_model/src/new/lstm_modeling_functions.py
+++ 03_model/src/new/lstm_modeling_functions.py
@@ -439,7 +439,7 @@ def run_multi_site_model_c(netcdf_loc, config_loc, site_no_list, station_nm_list
             concat_model_data = pickle.load(input_data)
         with open(os.path.join(input_file_loc,'n_means_stds'), 'rb') as means_stds:
             n_means_stds = pickle.load(means_stds)
-        print('Reading input data from '+ out_dir)
+        print('Reading input data from '+ input_file_loc)
     else:         
         #prepare the data
         concat_model_data, n_means_stds = ppf.full_prepare_multi_site_data(netcdf_loc, config_loc, site_no_list, station_nm_list, out_dir, fine_tune, weights_dir)
@@ -522,7 +522,6 @@ def wrapper_run_multi_site_model_c(run_config_loc):
     #oos_sites = run_config['oos_sites']
     multi_site = True
     
-    #model_run_dir = os.path.join('03_model/out/multi_site', model_run_id)
     input_file_loc = os.path.join('03_model/out/multi_site',model_run_id)
     
     for j in range(n_reps):
@@ -721,3 +720,48 @@ def wrapper_single_site_model_hyperparameter_tuning(run_config_loc):
                 
             all_sites_results_df = pd.DataFrame(all_sites_results_list)
             all_sites_results_df.to_csv(os.path.join(model_rep_HP_dir,"AllSitesModelResults.csv"))
+            
+def wrapper_run_cluster_model(run_config_loc):
+    with open(run_config_loc) as stream:
+        run_config = yaml.safe_load(stream) 
+        
+    site_info = pd.read_csv(run_config['site_info_loc'],  dtype = {'site_no':str})    
+    
+    model_run_id = run_config['model_run_id']
+    netcdf_loc = run_config['netcdf_loc']
+    config_loc = run_config['config_loc']
+    read_input_data_from_file = run_config['read_input_data_from_file']
+    n_reps = run_config['n_reps']
+    train_model = run_config['train_model']
+    save_results_csv = run_config['save_results_csv']
+    hp_tune = run_config['hp_tune']
+    hp_tune_vals = run_config['hp_tune_vals']
+    fine_tune = run_config['fine_tune']
+    weights_dir = run_config['weights_dir']
+    #oos_sites = run_config['oos_sites']
+    multi_site = True
+    
+    input_file_loc = os.path.join('03_model/out/multi_site',model_run_id)
+    
+    for j in range(n_reps):
+        rep = 'Rep_0'+str(j)
+         
+        for group in np.sort(site_info.cluster_01.unique()):
+            print(group,'of ',len(site_info.cluster_01.unique()),' groups')
+            site_no_list = site_info[site_info['cluster_01'] == group].site_no
+            station_nm_list = site_info[site_info['cluster_01'] == group].station_nm
+            run_id = os.path.join(model_run_id, rep,  'Cluster_0'+str(group))
+            out_dir = os.path.join('03_model/out/multi_site', model_run_id, rep,  'Cluster_0'+str(group))
+            if j == 0:
+                read_input_data_from_file = False
+            else:
+                read_input_data_from_file = True
+                input_file_loc = os.path.join('03_model/out/multi_site',model_run_id,'Rep_00','Cluster_0'+str(group))
+            
+            run_multi_site_model_c(netcdf_loc, config_loc, site_no_list, station_nm_list, 
+                       read_input_data_from_file, input_file_loc, out_dir, run_id,
+                       train_model, multi_site, fine_tune, weights_dir, hp_tune, hp_tune_vals, save_results_csv)
+            print('-----------------------------------------------------------------------')
+            print('Training group ', group, ' of 8 sites | replicate #', str(n_reps+1))
+            print('-----------------------------------------------------------------------')
+
