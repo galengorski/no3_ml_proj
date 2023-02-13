fix expected gradients function

04_analysis/src/expected_gradients_functions.py
@@ -164,7 +164,7 @@ def select_site(site, run_id, batch_size = 512, device = 'cpu', shuffle = True):
     
     return concat_model_data, site_dates, train_loader_site, train_loader_site_full
 
-def calc_expected_gradients(model_config_loc, run_id, weights_loc, site):
+def calc_expected_gradients(model_config_loc, run_id, site, n_reps):
     '''
     Parameters
     ----------
@@ -172,8 +172,6 @@ def calc_expected_gradients(model_config_loc, run_id, weights_loc, site):
         file path; location of the model config yaml file
     run_id: str
         model run directory
-    weights_loc : str
-        file paht; location of the trained weights file
     site : str
         site_no of the site of interest
 
@@ -186,21 +184,44 @@ def calc_expected_gradients(model_config_loc, run_id, weights_loc, site):
 
     '''
     
-    feat_list, model = initialize_model(model_config_loc, weights_loc)
-    concat_model_data, site_dates, site_data, full_data = select_site(site, run_id)
     
-    #site_data.dataset.X has dimension [n_obs, seq_len, n_features]
-    x_data_in = site_data.dataset.X
-    x_set = full_data.dataset.X
     n_samples = 200
-    print("Calculating expected gradients for "+site)
-    egs = expected_gradients_lstm(x_data_in, x_set, model, n_samples, temporal_focus=None)
+    
+    
+    
+    egs_all_reps_list = list()
+    for j in range(n_reps):
+        rep = 'Rep_0'+str(j)
+        weights_loc = os.path.join('03_model/out/multi_site',run_id,rep,'model_weights.pt')
+        if j == 0:
+            print("Preparing data: "+rep)
+            feat_list, model = initialize_model(model_config_loc, weights_loc)
+            concat_model_data, site_dates, site_data, full_data = select_site(site, run_id)
+
+            #site_data.dataset.X has dimension [n_obs, seq_len, n_features]
+            x_data_in = site_data.dataset.X
+            x_set = full_data.dataset.X
+            print("Calculating expected gradients for "+site)
+            egs = expected_gradients_lstm(x_data_in, x_set, model, n_samples, temporal_focus=None)
+
+        else:
+            print("Using prepared data: "+ rep)
+            print("Calculating expected gradients for "+site)
+            egs = expected_gradients_lstm(x_data_in, x_set, model, n_samples, temporal_focus=None)
+            
+        egs_all_reps_list.append(egs) 
+        
+        #stack the resulting list into a numpy array
+    egs_all_reps_np = np.stack(egs_all_reps_list, axis = 0)
+    #take the mean of all reps
+    egs_np = np.mean(egs_all_reps_np, axis = 0)
+    eg_df = pd.DataFrame(egs_np[:,0,:], columns = feat_list[1:], index = site_dates)
         
     #inputs_np = x_data_in.detach().numpy()
     #inputs_df = pd.DataFrame(inputs_np[:,0,:], columns = feat_list[1:], index = site_dates)
     
     #eg_df = pd.DataFrame(egs[:,0,:], columns = feat_list[1:], index = site_dates)
-    return egs
+    return eg_df
 
 def fetch_predictions(site, reps):
     ss_rep_all = pd.read_csv('03_model/out/single_site/Run_00_Full/Rep_00/'+site+'/ModelResults.csv', parse_dates = True, index_col = 'DateTime')
@@ -224,6 +245,19 @@ def fetch_predictions(site, reps):
     return model_preds_mean_ss, model_preds_mean_ms
 
 def calc_expected_gradients_all_sites(run_config_loc):
+    '''
+    wrapper function that calculates expected gradients across n replicates and saves them to a csv for each site
+
+    Parameters
+    ----------
+    run_config_loc : str
+        location of run config yaml file
+
+    Returns
+    -------
+    None.
+
+    '''
     with open(run_config_loc) as stream:
         run_config = yaml.safe_load(stream)  
 
@@ -232,17 +266,6 @@ def calc_expected_gradients_all_sites(run_config_loc):
     site_info = pd.read_csv(run_config['site_info_loc'],  dtype = {'site_no':str}) 
     n_reps = run_config['n_reps']
     
-    for site in site_info.site_no.unique():
-        egs_all_reps_list = list()
-        for j in range(n_reps):
-            rep = 'Rep_0'+str(j)
-            weights_loc = os.path.join('03_model/out/multi_site',run_id,rep,'model_weights.pt')
-            egs = calc_expected_gradients(model_config_loc, run_id, weights_loc, site)
-            
-            egs_all_reps_list.append(egs_np_rep)
-    
-        #stack the resulting list into a numpy array
-        egs_all_reps_np = np.stack(egs_all_reps_list, axis = 0)
-        #take the mean of all reps
-        egs_np = np.mean(egs_all_reps_np, axis = 0)
-        eg_df.to_csv('04_analysis/out/EG_sites/multi_site_07_'+site+'.csv')
+    for site in site_info.site_no.unique()[0:2]:
+        eg_df = calc_expected_gradients(model_config_loc, run_id, site, n_reps)
+        eg_df.to_csv('04_analysis/out/EG_sites/multi_site_01_'+site+'.csv')