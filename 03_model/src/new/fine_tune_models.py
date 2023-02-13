4f3461287000ed8cba174aee98cca679bebbaa60
Galen Gorski <32860978+galengorski@users.noreply.github.com>
2023-02-13 10:47:11 -0500

    add sys.path.append to locate eg module

--- 03_model/src/new/fine_tune_models.py
+++ 03_model/src/new/fine_tune_models.py
@@ -6,6 +6,8 @@ Created on Thu Jan 26 14:47:54 2023
 @author: galengorski
 """
 #%%
+import sys
+sys.path.append("Documents/GitHub/no3_ml_proj/04_analysis/src")
 import lstm_modeling_functions as lmf
 import expected_gradients_functions as egf
 #%%
